d6804e46a64df9f4bcfdb45b89a90c0286b74a26
rovm <rovm92@naver.com>
2020-04-29 15:56:15 +0900

    Create numCalcu.py
    정수 계산하기
    
    제곱, 사칙연산, 나머지 , 객체타입, 진법
    
    #

---
+++ numCalcu.py
@@ -0,0 +1,56 @@
+#정수 계산하기
+a = 1 + 1
+print(a) #2
+
+a = 1 - 2
+print(a) # -1
+
+a = 5 / 2
+print(a) # 2.5
+
+a = 4 / 2
+print(a) # 2.0
+
+a = 5.5 // 2
+print(a) # 2
+
+a = 4 // 2
+print(a) # 2
+
+a = 4.1 // 2.1
+print(a) # 1.0
+
+a = 5 % 2
+print(a) # 1
+
+a = 2 ** 10
+print(a) # 1024
+
+a = int(3.3)
+print(a) # 3
+
+a = int(5/2)
+print(a) # 2
+
+a = int('10')
+print(a) # 10
+
+a = type(10)
+print(a) # <class 'int'>
+
+a = divmod(5,2)
+print(a) # (2,1)
+
+a, b = divmod(5,2)
+print(a)
+print(b)
+print(a, b)
+
+a = 0b110
+print(a) # 6
+
+a = 0o10
+print(a) # 8
+
+a = 0xf
+print(a) # 15
