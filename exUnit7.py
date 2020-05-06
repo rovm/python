d1f93cf25015b6ebf7f2c68a2b0638a7f028a575
rovm <rovm92@naver.com>
2020-05-06 15:07:46 +0900

    출력 방법 알아보기
    값을 여러 개 출력하기
    
    #

---
+++ exUnit7.py
@@ -0,0 +1,9 @@
+#값을 여러 개 출력하기
+print(1, 2, 3)
+print('Hello', 'Python')
+
+#sep로 값 사이에 문자 넣기
+print(1, 2, 3, sep=', ')
+print(4, 5, 6, sep=',')
+print('Hello', 'Python', sep='')
+print(1920, 1080, sep='x')
