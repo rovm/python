42b244d5ccec7e2878257d5f4fb9c229eeed1171
rovm <rovm92@naver.com>
2020-05-06 14:47:43 +0900

    변수와 입력 사용하기
    입력 값을 변수에 저장하기

--- exUnit6.py
+++ exUnit6.py
@@ -29,3 +29,36 @@ x = None #None = Null
 print(x) 
 
 #변수로 계산하기
+a = 10
+b = 20
+c = a + b
+print(c)
+
+#산술 연산 후 할당 연산자 사용하기
+a = 10
+print(a + 20)
+print(a)
+
+a = 10
+a = a + 20
+print(a)
+
+a = 10
+a += 20
+print(a)
+
+#input 함수 사용하기
+
+
+x = -10
+print(+x)
+print(-x)
+
+#x = input()
+# C언어의 scanf 같은 기능임
+
+#두 숫자의 합 구하기
+a = int(input('1번째 수'))
+b = int(input('2번째 수'))
+print(a+b)
+print(type(a))
