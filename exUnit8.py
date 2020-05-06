78e3c5575634ad033508be4ff71a00fc964b55cd
rovm <rovm92@naver.com>
2020-05-06 15:24:43 +0900

    불과 비교, 논리 연산자 알아보기
    불과 비교 연산자 사용하기
    
    #

---
+++ exUnit8.py
@@ -0,0 +1,29 @@
+#불과 비교 연산자 사용
+print(True)
+print(False)
+
+#비교 연산자의 판단 결과
+print(3>1)
+
+#숫자가 같은지 다른지 비교하기
+print(10 == 10)
+print(10 != 5)
+
+#문자열이 같은지 다른지 비교하기
+print('Python' == 'Python')
+print('Python' == 'python')
+print('Python' != 'python')
+
+#부등호 사용하기
+print(10 > 20)
+print(10 < 20)
+print(10 >= 20)
+print(10 <= 20)
+
+#객체가 같은지 다른지 비교하기
+print(1 == 1.0)
+print(1 is 1.0)
+print(1 is not 1.0)
+#id 값을 사용하여 비교
+print(id(1))
+print(id(1.0))
