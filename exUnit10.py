31ed542c66915161b121c7cb6f4ee24b0bba94bf
rovm <rovm92@naver.com>
2020-05-11 15:49:28 +0900

    리스트와 튜플 사용하기
    리스트만들기

---
+++ exUnit10.py
@@ -0,0 +1,31 @@
+#리스트와 튜플 사용하기
+a = 10
+b = 20
+print(a, b, sep=',')
+
+a = [38, 21, 53, 62, 19]
+print(a)
+
+#리스트에 여러가지 자료형 저장하기
+person = ['james',17 ,175.3 ,True]
+print(person)
+
+#빈 리스트 만들기
+a = []
+print(a)
+b =list()
+print(b)
+
+#range를 사용하여 리스트 만들기
+print(range(10))
+a = list(range(10))
+print(a)
+
+b = list(range(5,12))
+print(b)
+
+c = list(range(-4, 10, 2))
+print(c)
+
+d = list(range(10, 0, -1))
+print(d)
