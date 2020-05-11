0f08a84875c1f9ea90ec24246eae1c51eb2bb378
rovm <rovm92@naver.com>
2020-05-11 16:47:10 +0900

    시퀀스 자료형 활용하기
    시퀀스 자료형의 공통 기능 사용하기

---
+++ exUnit11.py
@@ -0,0 +1,18 @@
+#특정 값이 있는지 확인하기
+a = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+print(30 in a)
+print(100 in a)
+print(100 not in a)
+print(30 not in a)
+
+print(43 in (38, 76, 43, 62, 19))
+print(1 in range(10))
+print('P' in 'Hello, Python')
+
+a = [0, 10, 20, 30]
+b = [9, 8, 7, 6]
+print(a + b)
+
+print([0, 10, 20, 30] * 3)
+
+print('Hello, ' * 3)
