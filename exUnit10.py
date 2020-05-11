b165c538284f067d8de6d9ef3ef4d2f50eaa3335
rovm <rovm92@naver.com>
2020-05-11 16:00:10 +0900

    리스트와 튜플 사용하기
    튜플 사용하기

--- exUnit10.py
+++ exUnit10.py
@@ -29,3 +29,41 @@ print(c)
 
 d = list(range(10, 0, -1))
 print(d)
+
+#튜플 사용하기
+a = (38, 21, 53, 62, 19)
+print(a)
+
+a = 38, 21, 53, 62, 19
+print(a)
+
+person = ('james', 17, 175.3, True)
+print(person)
+
+#range를 사용하여 튜플 만들기
+a = tuple(range(10))
+print(a)
+
+b = tuple(range(5, 12))
+print(b)
+
+c = tuple(range(-4, 10, 2))
+print(c)
+
+#튜플을 리스트로 만들고 리스트를 튜플로 만들기
+a = [1, 2, 3]
+print(tuple(a))
+
+b = (4, 5, 6)
+print(list(b))
+
+print(list('Hello'))
+print(tuple('Hello'))
+a, b, c = [1, 2, 3]
+print(a, b, c)
+d, e, f = (4, 5, 6)
+print(d, e, f)
+
+x = input().split()
+a, b = x
+print(a, b)
