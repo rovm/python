a9aabdbd5a58430fd793e19e64371e28bbb39f82
rovm <rovm92@naver.com>
2020-05-14 09:33:00 +0900

    시퀀스 자료형 활용하기
    슬라이스 사용하기

--- exUnit11.py
+++ exUnit11.py
@@ -59,3 +59,54 @@ print(a)
 #del로 요소 삭제 하기
 del a[2]
 print(a)
+
+#슬라이스 사용하기
+a = [0,10,20,30,40,50,60,70,80,90]
+print(a[0:4])
+print(a[0:10])
+print(a[1:0])
+print(a[1:2])
+
+#리스트의 중간 부분 가져오기
+print(a[4:7])
+print(a[4:-2])
+print(a[2:8:3])
+print(a[2:9:3])
+print(a[:7])
+print(a[7:])
+print(a[:])
+print(a[0:7:2])
+print(a[7::2])
+print(a[::2])
+
+#len 응용하기
+print(a[0:len(a)])
+print(a[:len(a)])
+
+#튜플 range 문자열에 슬라이스 적용하기
+b = (0, 10, 20, 30, 40, 50, 60, 70, 80, 90)
+print(b[4:7])
+print(b[4:])
+print(b[:7:2])
+
+r = range(10)
+print(r[0:10])
+print(r[4:7])
+print(r[4:10])
+print(r[:7:2])
+print(list(r[:7:2]))
+
+hello = 'Hello, world!'
+print(hello[2:9])
+print(hello[2:])
+print(hello[:9:2])
+
+#슬라이스에 요소 할당하기
+a = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+#a[2:5] = ['a','b','c']
+#a[2:5] = ['a']
+#a[2:5] = ['a','b','c','d']
+#a[2:8:2] = ['a','b','c']
+#del a[2:5]
+del a[2:8:2]
+print(a)
