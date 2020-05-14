b0626548bbc37e4fc2a647d1bd458a0cc30da9f5
rovm <rovm92@naver.com>
2020-05-14 10:35:17 +0900

    elif를 사용하여 여러 방향으로 분기하기
    elif 사용하기

---
+++ exUnit15.py
@@ -0,0 +1,27 @@
+#elif 사용하기
+#if 조건식:
+#     코드1
+#elif 조건식:
+#     코드2
+
+x = 20
+if x == 10:
+    print('x는 10입니다.')
+elif x == 20:
+    print('x는 20입니다.')
+
+#if, elif, else 모두 사용하기
+#if 조건식:
+#    코드1
+#elif 조건식:
+#    코드2
+#else:
+#    코드3
+
+x = 30
+if x == 10:
+    print('x는 10입니다.')
+elif x == 20:
+    print('x는 20입니다.')
+else:
+    print('10, 20이 아닙니다.')
