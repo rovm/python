b77b70e130815292482a3e16155208c700701bff
rovm <rovm92@naver.com>
2020-05-14 10:03:01 +0900

    딕셔너리 사용하기
    딕셔너리의 키에 접근한고 값 할당하기

--- exUnit12.py
+++ exUnit12.py
@@ -25,3 +25,20 @@ lux3 = dict([('health', 490), ('mana', 334), ('melee', 550), ('armor', 18.72)])
 print(lux3)
 lux4 = dict({'health': 490, 'mana': 334, 'melee': 550, 'armor': 18.72})
 print(lux4)
+
+#딕셔너리의 키에 접근하고 값 할당하기
+lux = {'health': 490, 'mana': 334, 'melee': 550, 'armor': 18.72}
+print(lux['health'])
+print(lux['mana'])
+
+#딕셔너리의 키에 값 할당하기
+lux['health'] = 2037
+lux['mana'] = 1184
+print(lux)
+print('health' in lux)
+print('attack_speed' in lux)
+print('attack_speed' not in lux)
+print('health' not in lux)
+lux['aaa'] = 123
+print(lux)
+print(len(lux))
