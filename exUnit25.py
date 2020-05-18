0bc5ca098998cf814a18cd25cab1f782b8b2a2a4
rovm <rovm92@naver.com>
2020-05-18 09:15:29 +0900

    딕셔너리 응용하기
    딕셔너리 안에서 딕셔너리 사용하기

--- exUnit25.py
+++ exUnit25.py
@@ -82,3 +82,29 @@ print()
 x = {'a': 10, 'b': 20, 'c': 30, 'd': 40}
 x = {key: value for key, value in x.items() if value != 20}
 print(x)
+
+#딕셔너리 안에서 딕셔너리 사용하기
+terrestrial_planet = {
+    'Mercury': {
+        'mean_radius': 2439.7,
+        'mass': 3.3022E+23,
+        'orbital_period': 87.969
+    },
+    'Venus': {
+        'mean_radius': 6051.8,
+        'mass': 4.8676E+24,
+        'orbital_period': 224.70069,
+    },
+    'Earth': {
+        'mean_radius': 6371.0,
+        'mass': 5.97219E+24,
+        'orbital_period': 365.25641,
+    },
+    'Mars': {
+        'mean_radius': 3389.5,
+        'mass': 6.4185E+23,
+        'orbital_period': 686.9600,
+    }
+}
+ 
+print(terrestrial_planet['Venus']['mean_radius'])
