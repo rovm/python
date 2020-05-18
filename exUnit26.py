007edc873ab2fdc9d3d611fdb405fdd1114548a3
rovm <rovm92@naver.com>
2020-05-18 10:29:17 +0900

    세트사용하기
    반목문으로 세트의 요소를 모두 출력하기
    세트 표현식 사용하기

--- exUnit26.py
+++ exUnit26.py
@@ -122,3 +122,17 @@ b = a.copy()
 b.add(5)
 print(a)
 print(b)
+
+#반복문으로 세트의 요소를 모두 출력하기
+a = {1,2,3,4}
+for i in a:
+    print(i)
+
+for i in {1,2,3,4}:
+    print(i)
+
+#세트 표현식 사용하기
+a = {i for i in 'apple'}
+print(a)
+a = {i for i in 'pineapple' if i not in 'apl'}
+print(a)
