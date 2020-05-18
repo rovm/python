3e6cd9524d6b86e6f99ff72966784fd40a0bb754
rovm <rovm92@naver.com>
2020-05-18 10:57:42 +0900

    파일 사용하기
    파이썬 객체를 파일에 저장하기, 가져오기

--- exUnit27.py
+++ exUnit27.py
@@ -29,3 +29,30 @@ with open('hello.txt','r') as file:
     while line != '':
         line = file.readline()
         print(line.strip('\n'))
+
+#파이썬 객체를 파일에 저장하기
+import pickle
+ 
+name = 'james'
+age = 17
+address = '서울시 서초구 반포동'
+scores = {'korean': 90, 'english': 95, 'mathematics': 85, 'science': 82}
+ 
+with open('james.p', 'wb') as file:    # james.p 파일을 바이너리 쓰기 모드(wb)로 열기
+    pickle.dump(name, file)
+    pickle.dump(age, file)
+    pickle.dump(address, file)
+    pickle.dump(scores, file)
+
+#파일에서 파이썬 객체 읽기
+import pickle
+ 
+with open('james.p', 'rb') as file:    # james.p 파일을 바이너리 읽기 모드(rb)로 열기
+    name = pickle.load(file)
+    age = pickle.load(file)
+    address = pickle.load(file)
+    scores = pickle.load(file)
+    print(name)
+    print(age)
+    print(address)
+    print(scores)
