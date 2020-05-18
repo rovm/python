5ee7ac1d5696c09e58ddf10f0e75891f4fb3a7d2
rovm <rovm92@naver.com>
2020-05-18 10:00:31 +0900

    세트 사용하기
    집합 연상 사용하기

--- exUnit26.py
+++ exUnit26.py
@@ -20,3 +20,59 @@ print(b)
 
 c = set()
 print(c)
+
+#집합연산 사용하기
+a = {1,2,3,4}
+b = {3,4,5,6}
+print(a|b)
+print(set.union(a,b))
+print(a&b)
+print(set.intersection(a,b))
+print(a-b)
+print(set.difference(a,b))
+print(a^b)
+print(set.symmetric_difference(a,b))
+
+#집합 연산 후 할당 연산자 사용하기
+a = {1,2,3,4}
+a |= {5}
+a.update({6})
+print(a)
+
+a = {1,2,3,4}
+a &= {0,1,2,3,4}
+print(a)
+a = {1,2,3,4}
+a.intersection_update({0,1,2,3,4})
+print(a)
+
+a = {1,2,3,4}
+a -= {3}
+print(a)
+a = {1,2,3,4}
+a.difference_update({3})
+print(a)
+
+a = {1, 2, 3, 4}
+a ^= {3, 4, 5, 6}
+print(a)
+a = {1, 2, 3, 4}
+a.symmetric_difference_update({3, 4, 5, 6})
+print(a)
+
+#부분 집합과 상위집합 확인하기
+a = {1,2,3,4}
+print(a <= {1,2,3,4})
+print(a.issubset({1, 2, 3, 4, 5}))
+print(a < {1, 2, 3, 4, 5})
+print(a >= {1, 2, 3, 4})
+print(a.issuperset({1, 2, 3, 4}))
+print(a>{1,2,3})
+
+#세트가 같은지 다른지 확인하기
+a = {1,2,3,4}
+print(a == {1,2,3,4})
+print(a == {4,3,2,1})
+print(a != {1,2,3,4})
+
+세트가 겹치지 않는지 확인하기
