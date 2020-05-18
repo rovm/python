20dc810c9b088c75b5b782daad931add0ea33a29
rovm <rovm92@naver.com>
2020-05-18 09:30:10 +0900

    세트만들기
    세트만들기

---
+++ exUnit26.py
@@ -0,0 +1,22 @@
+#세트 만들기
+fruits = {'strawberry', 'grape', 'orange', 'pineapple', 'cherry'}
+print(fruits)
+
+fruits = {'orange', 'orange', 'cherry'}
+print(fruits)
+
+fruits = {'strawberry', 'grape', 'orange', 'pineapple', 'cherry'}
+print('orange' in fruits)
+print('peach' in fruits)
+print('orange' not in fruits)
+print('peach' not  in fruits)
+
+#set를 사용하여 세트 만들기
+a = set('apple')
+print(a)
+
+b = set(range(5))
+print(b)
+
+c = set()
+print(c)
