9df9e2bb4b7d9be96d7b5bd2bb15b4bd723c7855
rovm <rovm92@naver.com>
2020-05-18 11:37:21 +0900

    함수 사용하기
    함수의 호출 과정 알아보기

--- exUnit29.py
+++ exUnit29.py
@@ -44,3 +44,18 @@ def add_sub(a,b):
     return a+b, a-b
 x,y = add_sub(10,20)
 print(x, y)
+
+#함수의 호출 과정 알아보기
+def mul(a, b):
+    c = a * b
+    return c
+ 
+def add(a, b):
+    c = a + b
+    print(c)
+    d = mul(a, b)
+    print(d)
+ 
+x = 10
+y = 20
+add(x, y)
